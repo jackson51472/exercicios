d9e0adef5c1048c71642be390a86f4efd072e368
jackson51472 <53409735+jackson51472@users.noreply.github.com>
2022-09-29 19:47:46 -0300

    Create Programa.java

---
+++ 2022.02/Atividades Implementacao classe Data/Programa.java
@@ -0,0 +1,21 @@
+public class Programa {
+    public static void main(String[] args) {
+        Data d1 = new Data(24,04,2000);
+        Data d2 = new Data(25,04,2002);
+        Data d3 = new Data(26,04,2021);
+        System.out.println("===========================================================================================");
+        d1.normalData();
+        d2.normalData();
+        d3.normalData();
+        System.out.println("===========================================================================================");
+        d1.parametro();
+        d2.parametro();
+        d3.parametro();
+        System.out.println("===========================================================================================");
+        d1.isBissexto();
+        d2.isBissexto();
+        d3.isBissexto();
+
+
+    }
+}
