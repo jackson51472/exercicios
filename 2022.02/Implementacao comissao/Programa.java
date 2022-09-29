1aff7eacd378e8253b27ee7fb99eab88469f120a
jackson51472 <53409735+jackson51472@users.noreply.github.com>
2022-09-29 19:56:07 -0300

    Create Programa.java

---
+++ 2022.02/Implementacao comissao/Programa.java
@@ -0,0 +1,12 @@
+public class Programa {
+    public static void main(String[] args) {
+        Funcionario a1 = new Funcionario(12,12390);
+        Funcionario a2 = new Funcionario(15,6770);
+        Funcionario a3 = new Funcionario(23,15500);
+        Funcionario a4 = new Funcionario(44,45600);
+        a1.definicoes();
+        a2.definicoes();
+        a3.definicoes();
+        a4.definicoes();
+    }
+}
